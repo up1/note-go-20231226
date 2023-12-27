1815222ef5aa9c874f4e87766b60e105714e01e9
up1 <somkiat.p@gmail.com>
2023-12-27 16:23:51 +0700

    Add benchmark

---
+++ demo_echo/hello_bench_test.go
@@ -0,0 +1,18 @@
+package demo_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkHelloV1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprint("Hello, World!")
+	}
+}
+
+func BenchmarkHelloV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("Hello, World!")
+	}
+}
